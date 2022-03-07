3363b8c607db63b150a7274a8e75e406a6037477
Thuta <thutaminthway2001@gmail.com>
2022-03-07 12:00:28 +0630

    styling project

--- resources/views/components/main.blade.php
+++ resources/views/components/main.blade.php
@@ -12,23 +12,23 @@
 
 <body class="font-sans bg-gray-900 text-white">
     <nav class="border-b border-grey-800">
-        <div class="container mx-auto flex items-center justify-between px-4 py-6">
-            <ul class="flex items-center">
+        <div class="container mx-auto px-4 flex flex-col md:flex-row items-center justify-between px-4 py-6">
+            <ul class="flex flex-col md:flex-row items-center">
                 <li>
                     <a href="#"><span><i class="fa-solid fa-film mx-3"></i>Movie App</span></a>
                 </li>
-                <li class="ml-16">
+                <li class="md:ml-16 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Movies</a>
                 </li>
-                <li class="ml-6">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">TV shows</a>
                 </li>
-                <li class="ml-6">
+                <li class="md:ml-6 mt-3 md:mt-0">
                     <a href="#" class="hover:text-gray-300">Actors</a>
                 </li>
             </ul>
-            <div class="flex items-center">
-                <div class="relative">
+            <div class="flex flex-col md:flex-row items-center">
+                <div class="relative mt-3 md:mt-0">
                     <input type="text"
                         class="bg-gray-800 rounded-full text-sm w-64 px-4 py-1 pl-8 focus:outline-none focus:shadow-outline"
                         placeholder="Search">
@@ -36,7 +36,7 @@
                         <span><i class="fa-solid fa-magnifying-glass w-4 text-gray-500 mt-1 ml-2"></i></span>
                     </div>
                 </div>
-                <div class="ml-4">
+                <div class="md:ml-4 mt-3 md:mt-0">
                     <img src="/img/avatar.jpg" alt="avater" class="rounded-full w-8 h-8">
                 </div>
             </div>
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,158 @@
 <x-main>
-    <div>
-        Index of movies
+    <div class="container mx-auto px-4 pt-16">
+        <div class="popular-movies">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Popular Movies</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="now-playing-movies py-24">
+            <h2 class="uppercase tracking-wider text-orange-500 text-lg font-semibold">Now Playing Movies</h2>
+            <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+                <div class="mt-8">
+                    <a href="#">
+                        <img src="/img/parasite.jpg" alt=""
+                            class="hover:opacity-75 transition ease-in-out duration-150">
+                    </a>
+                    <div class="mt-2">
+                        <a href="#" class="text-lg mt-2 hover:text-gray-400">Parasite</a>
+                        <div class="flex items-center text-gray-400 text-sm">
+                            <span><i class="fa-solid fa-star text-orange-500"></i></span>
+                            <span class="ml-1">85%</span>
+                            <span class="ml-2"> | </span>
+                            <span>Feb 10,2020</span>
+                        </div>
+                        <div class="text-gray-400 text-sm">
+                            Action, Thriller, Comedy
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </x-main>
